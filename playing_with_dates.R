693b4f999f0c2daec9f50ad386146f8743bdc1e3
Bart Kastermans <kasterma@kasterma.net>
2015-05-19 14:38:21 +0200

    code for time conversion surprise

--- playing_with_dates.R
+++ playing_with_dates.R
@@ -7,3 +7,13 @@ library(lubridate)
 
 class(ymd("2015-03-10"))
 seq(from = ymd("2015-03-10"), to = ymd("2015-03-15"), by = "day")
+
+(xx <- Sys.time() + 1:3)
+as.vector(xx)
+dput(xx)
+(yy <- .Internal(Sys.time()))
+dput(yy)
+class(yy)
+is.integer(yy)
+is.double(yy)
+dput(as.integer(yy))
