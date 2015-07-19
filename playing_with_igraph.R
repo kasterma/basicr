4ffe8d6dd0dacd1b14397ee4f458f9e99a6ac65f
Bart Kastermans <kasterma@kasterma.net>
2015-07-19 10:54:25 +0200

    add breath first

--- playing_with_igraph.R
+++ playing_with_igraph.R
@@ -22,7 +22,7 @@ depth_first <- function(g, v) {
   get_nbd <- function(x) neighbors(g, x)
   q <- v
   added <- v
-  proc <- c()
+  proc <- V(g)[FALSE]   # empty list of vertices from this graph
   while(length(q) > 0) {
     n <- q[1]
     q <- q[-1]
@@ -42,3 +42,28 @@ depth_first <- function(g, v) {
 }
 
 depth_first(g, V(g)["B"])
+
+#' do a breadth first traversal of g starting at v
+breadth_first <- function(g, v) {
+  q <- v
+  added <- v
+  proc <- V(g)[FALSE]  # empty list of vertices from this graph
+  while(length(q) > 0) {
+    n <- q[1]
+    q <- q[-1]
+    
+    proc <- c(proc, n)
+    
+    nbd <- neighbors(g, n)
+    # only add any element once (or finite no times if multiple edges)
+    nbd <- nbd[!nbd %in% added]
+    if(length(nbd) != 0) {
+      q <- c(q, nbd)
+      added <- c(added, nbd)
+    }
+    
+  }
+  proc
+}
+
+breadth_first(g, V(g)["A"])
