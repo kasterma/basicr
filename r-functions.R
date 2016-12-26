72063d759d7e1cc9ed26badd65ad4a099b36f532
Bart Kastermans <kasterma@kasterma.net>
2016-12-26 09:21:18 +0100

    add functions

--- r-functions.R
+++ r-functions.R
@@ -1,2 +1,10 @@
-data.matrix
-mapply  (and its use for zipping)
+# Convert a Data Frame to a Numeric Matrix
+data.matrix(data.frame(x = c(1,2,3), y = 4))
+data.matrix(data.frame(x = c(1,2,3), y = c("a", "b", "c")))
+data.matrix(data.frame(x = c(1,2,3), y = c("a", "b", "c"), stringsAsFactors = FALSE))
+
+match(c(1,2,3), c(0,0,1,3,3))
+match(c(0,1,2), c(0,1,2), incomparables = c(0,1), nomatch = -1)
+
+# mapply use for zipping
+mapply(c, c(11,22,33), c(44,55,66), SIMPLIFY = FALSE)
